65b325c1b746dc2707ec1403c24ec69ea700df86
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-07-31 10:59:02 -0500

    adds primer react

--- src/Datepicker/Datepicker.tsx
+++ src/Datepicker/Datepicker.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import { Box, themeGet } from "@primer/react";
 
 type DatepickerProps = {
   children: React.ReactNode;
 };
 
 export const Datepicker = ({ children }: DatepickerProps) => {
-  return <StyledThing>{children}</StyledThing>;
+  return <Wrapper>{children}</Wrapper>;
 };
 
-const StyledThing = styled.div`
-  background: black;
-  color: white;
+const Wrapper = styled(Box)`
+  background: ${themeGet("colors.canvas.default")};
+  color: ${themeGet("colors.fg.default")};
 `;
